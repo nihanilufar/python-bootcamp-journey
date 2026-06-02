a53b87b3e5db36fdd8474e8ba71f0d27bc475451
nihanilufar <nihanilufar00@gmail.com>
2026-06-02 12:42:07 +0530

    Q7.py

---
+++ work/Q7.py
@@ -0,0 +1,12 @@
+mark = int(input("Enter mark(0-100):  "))
+
+if  mark>=90:
+    print("A")
+elif  mark>=80:
+    print("B")
+elif mark>=70:
+    print("C")
+elif  mark>=60:
+    print("D")
+else:
+    print("F") 
